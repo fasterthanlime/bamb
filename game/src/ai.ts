bd9de6b62db7955656c409d6c5b5c0db26f3c420
Amos Wenger <amos@itch.io>
2018-12-02 12:53:36 +0100

    Introduce computeScore, use stateApplyMove to know if a move is legal

--- game/src/ai.ts
+++ game/src/ai.ts
@@ -1,5 +1,6 @@
 import { Game } from "./game";
 import { GameState, Move } from "./types";
+import { stateApplyMove } from "./state-apply-move";
 
 function calculateBestMove(game: Game, state: GameState): Move {
   let deck = state.decks[state.currentPlayer];
@@ -9,65 +10,57 @@ function calculateBestMove(game: Game, state: GameState): Move {
 
   // goes over all the deck cards
   for (let deckCard of deck.cells) {
-    if (deckCard.cardId) {
-      let card = game.cards[deckCard.cardId];
+    if (!deckCard.cardId) {
+      continue;
+    }
+    let card = game.cards[deckCard.cardId];
 
-      if (typeof card.value === "number") {
-        // checks the entire board to see if it can be placed
-        for (let x = 0; x < game.numRows; x++) {
-          for (let y = 0; y < game.numCols; y++) {
-            // Does this current cell already occupy a card? If not, try placing (TODO: modifiers)
-            if (!state.board.cells[x * game.numRows + y].cardId) {
-              let newState = game.boardSetCard(
-                state.board,
-                { row: x, col: y },
-                card.id,
-              );
+    // checks the entire board to see if it can be placed
+    for (let col = 0; col < game.numCols; col++) {
+      for (let row = 0; row < game.numRows; row++) {
+        let move = {
+          cardId: card.id,
+          player: state.currentPlayer,
+          placement: { col, row },
+        };
 
-              let tilesOwned = 0;
-              let valueOwned = 0;
-
-              for (let j = 0; j < newState.cells.length; j++) {
-                if (newState.cells[j].cardId) {
-                  let card = game.cards[newState.cells[j].cardId];
-                  if (j == x || j % y == 1) {
-                    if (card.player != state.currentPlayer) {
-                      tilesOwned++;
-                      valueOwned += card.value;
-                    }
-                  }
-                }
-              }
-
-              let newMove = {
-                cardId: card.id,
-                player: state.currentPlayer,
-                placement: { col: y, row: x },
-              };
-
-              let newScore = valueOwned;
-              newScore -= dist(7, game.boardSumCol(newState, y));
-              newScore -= dist(7, game.boardSumRow(newState, x));
-
-              console.log(`score for [${x}:${y}] is ${newScore}`);
-
-              if (newScore >= bestScore) {
-                bestMove = newMove;
-                bestScore = newScore;
-              }
-            }
-          }
+        // simulate playing this card
+        let newState = stateApplyMove(game, state, move);
+        if (newState == state) {
+          // not a legal move, keep searching
+          continue;
         }
-      } else if (typeof card.value === "string") {
-        // TODO: modifier cards.
+
+        let score = computeScore(game, newState, state.currentPlayer);
+        console.log(`score for [${row}:${col}] is ${score}`);
+        if (score > bestScore) {
+          bestMove = move;
+          bestScore = score;
+        }
       }
     }
   }
   return bestMove;
 }
 
-function dist(x: number, y: number): number {
-  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+function computeScore(game: Game, state: GameState, player: number): number {
+  let tilesOwned = 0;
+  let valueOwned = 0;
+
+  let { board } = state;
+  for (const cell of state.board.cells) {
+    if (!cell.cardId) {
+      continue;
+    }
+
+    let card = game.cards[cell.cardId];
+    if (card.player == player) {
+      tilesOwned++;
+      valueOwned += card.value;
+    }
+  }
+
+  return tilesOwned + valueOwned / 100;
 }
 
 export function processAI(game: Game) {
